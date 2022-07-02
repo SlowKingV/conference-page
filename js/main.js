fd48c6dd87431f6a9a0f0e53e2ebb6ec0945741f
Diego Luna <slowkingvii@gmail.com>
2022-07-02 08:28:30 -0500

    Add desktop styles for index page

--- js/main.js
+++ js/main.js
@@ -4,7 +4,26 @@ const menuBtn = document.getElementById('menu-btn');
 const menuClosers = Array.from(document.querySelectorAll('.nav-container .link'));
 menuClosers.push(mainContainer);
 
-menuBtn.addEventListener('click', () => { mainContainer.classList.toggle('move-out'); });
+const menuBtnToggle = () => {
+  const children = menuBtn.children;
+
+  if (mainContainer.classList.contains('move-out')) {
+    children[0].classList.add('d-none');
+    children[1].classList.remove('d-none');
+  } else {
+    children[0].classList.remove('d-none');
+    children[1].classList.add('d-none');
+  }
+}
+
+menuBtn.addEventListener('click', () => {
+  mainContainer.classList.toggle('move-out');
+  menuBtnToggle();
+});
+
 menuClosers.forEach((element) => {
-  element.addEventListener('click', () => { mainContainer.classList.remove('move-out'); }); 
+  element.addEventListener('click', () => {
+    mainContainer.classList.remove('move-out');
+    menuBtnToggle();
+  });
 });
